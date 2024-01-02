48ff3cdad934b249db1bb1a193d32f6dc6e28496
ummwhat <adi23arora@gmail.com>
2024-01-02 15:52:43 +0530

    requirements added

--- template.py
+++ template.py
@@ -30,14 +30,16 @@ list_of_files = [
 
 
 for filepath in list_of_files:
+    # windows uses backward slash for path
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-
+    # create directory if not present
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
-
+    
+    # create the file if not present
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
